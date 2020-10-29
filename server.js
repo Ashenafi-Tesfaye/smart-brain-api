a46a8756d912a14a2e80a90871e3a220aa9e59fd
Ashenafi-Tesfaye <65870089+Ashenafi-Tesfaye@users.noreply.github.com>
2020-10-29 06:04:21 -0700

    correct the can not get error

--- server.js
+++ server.js
@@ -24,13 +24,12 @@ const db=knex({
 
 const app=express();
 
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
 
 
-// app.get('/',(req, res)=>{
-// 	res.send(database.users);
-// })
+
+app.get('/',(req, res)=>{res.send('it is getting started')});
 
 app.post('/signin', (req,res)=>{signin.handleSignin(req,res,db,bcrypt)})
 
