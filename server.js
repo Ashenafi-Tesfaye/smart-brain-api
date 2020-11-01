57df6a1882fc13fea1924e0d4fda9d3e6fd3ed42
Ashenafi-Tesfaye <65870089+Ashenafi-Tesfaye@users.noreply.github.com>
2020-11-01 00:21:44 -0700

    rest the image link

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const knex=require('knex');
 const register=require('./controllers/register');
 const signin= require('./controllers/signin');
 const profile=require('./controllers/profile');
-const {handleImage,handleApiCall} = require('./controllers/image');
+const image = require('./controllers/image');
 
 
 
@@ -40,10 +40,9 @@ app.post('/register', (req,res)=>{register.handleRegister(req,res,db,bcrypt)})
 app.get('/profile/:id', (req,res)=>{profile.handleProfileGet(req,res,db)})
 
 app.put('/image', (req,res)=>{image.handleImage(req,res,db)})
-	//handleImage(req,res,db)})
-
-app.post('/imageurl', (req, res) => {image.handleApiCall(req,res)})
-	//.handleApiCall(req,res)})
+	
+app.post('/imageurl', (req, res) =>{image.handleApiCall(req,res)})
+	
 
 
 app.listen(process.env.PORT||3000, ()=>{
